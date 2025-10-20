efada4a23ac4edc9a4a9ac5aaccf79e04feae7c5
Aadil Brar <eternalsunrise@Eternalsunshine.local>
2025-10-20 15:19:21 +0800

    chore: configure static export for netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
